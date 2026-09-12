6bccf4024232c4229c103e44f165c256e8d24c23
Moha <flashmoah315@gmail.com>
2026-09-12 21:15:50 +0300

    Rebuilit log reg with OOP

--- Logistic Regression/logreg.py
+++ Logistic Regression/logreg.py
@@ -6,42 +6,42 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_sco
 from sklearn.preprocessing import StandardScaler
 #Predict whether someone will pass based on their study time
 
-#load data
-data = pd.read_csv("Logistic Regression/logistic_regression_level_2.csv")
-X = data[['study_hours','practice_tests','attendance_pct','sleep_hours']].to_numpy()
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-y = data['passed'].to_numpy()
-X_train, X_test, y_train, y_test = tt(X_scaled, y, test_size=0.2, random_state=42)
-#model
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+class LogisticRegression:
+    def __init__(self, learning_rate=0.0001, epochs=250):
+        self.lr = learning_rate
+        self.weights = None
+        self.bias = None
+        self.epochs = epochs
 
-b0 = 0
-b = np.zeros(4)
-alpha = 0.01
-for i in range(1000): #Gradient Descent
-    n = len(X_train)
-    y_p = sigmoid(b0 + X_train @ b)
-    db0 = (1/n)*np.sum(y_p - y_train)
-    db = (1/n)*(X_train.T  @ (y_p - y_train))
-    b0 -= alpha*db0
-    b -= alpha*db
-    error = -(1/len(y_train)) * np.sum(
-    y_test * np.log(y_p)
-    + (1-y_test) * np.log(1-y_p)
-)
-y_p = sigmoid(b0 + X_test @ b)
-pred = (y_p >= 0.5).astype(int)
-#BCE 
-error = -(1/len(y_test)) * np.sum(
-    y_test * np.log(y_p)
-    + (1-y_test) * np.log(1-y_p)
-)
-recall = recall_score(y_test, pred)
-fig, ax = plt.subplots(figsize=(6,6))
-plt.scatter(X_test, y_test, color='blue')
-plt.plot(y_p, color='orange', label='Model')
-plt.legend()
-plt.tight_layout()
-plt.show()
+    def _sigmoid(self, X):
+        z = np.dot(X, self.weights) + self.bias
+        return 1 / (1 + np.exp(-z))
+    
+    def fit(self, X, y):
+        num_samples, num_features = X.shape
+        self.bias = 0.0
+        self.weights = np.zeros(num_features)
+        for _ in range(self.epochs):
+            y_pred = self._sigmoid(X)
+            dw = (1 / num_samples) * np.dot(X.T, (y_pred - y))
+            db = (1 / num_samples) * np.sum(y_pred - y)
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        return self._sigmoid(X)
+
+data = pd.read_csv('Logistic Regression/logistic_regression_level_2.csv')
+X = data[['study_hours', 'practice_tests']]
+y = data['passed']
+X_train, X_test, y_train, y_test = tt(X, y, test_size=0.2)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+model = LogisticRegression(learning_rate=0.01, epochs=1000)
+model.fit(X_train, y_train)
+preds = model.predict(X_test)
+#evaluation
+
+
+    
