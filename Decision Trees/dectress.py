2e7bea53d8a2523930f5c2be674652c5a1f4d34c
Moha <flashmoah315@gmail.com>
2026-09-15 08:25:29 +0300

    Initilaised Node class and Model class for Decision Tree Classifier

---
+++ Decision Trees/dectress.py
@@ -0,0 +1,11 @@
+import numpy as np
+from collections import Counter
+
+#Helper class
+class Node:
+  def __init__(self):
+     pass
+  
+class DecisionTreeClassifier:
+    def __init__(self):
+        pass
