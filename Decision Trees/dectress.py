17147567593d18bb15b3fd9e1a0e6b51fd9a3732
Moha <flashmoah315@gmail.com>
2026-09-17 10:52:14 +0300

    Gng ok, js built a Node helper class, which we can then use to build our decision tree

--- Decision Trees/dectress.py
+++ Decision Trees/dectress.py
@@ -1,11 +1,25 @@
 import numpy as np
 from collections import Counter
 
-#Helper class
+#Building a single Node.
 class Node:
-  def __init__(self):
-     pass
+  def __init__(self, feature=None, threshold=None, left=None, right=None, label=None):
+     self.feature= feature
+     self.threshold = threshold
+     self.left = left
+     self.right = right
+     self.label = label
+
+     def _is_end_point(self): #check if node is a leaf
+        if self.left == self.right:
+           return self.label #returns prediction
+         
   
 class DecisionTreeClassifier:
-    def __init__(self):
-        pass
+    def __init__(self, node):
+       node = Node()
+
+    def predict(self, node, sample):
+       
+       
+
