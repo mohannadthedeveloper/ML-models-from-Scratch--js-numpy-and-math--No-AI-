6de0bd1c0ee67d509bfee0dbca9880a950a0cc08
Moha <flashmoah315@gmail.com>
2026-09-20 20:47:20 +0300

    Fixed logarithmic error, seriously js basic add  math. Also normalized pur logits with a softmax function that will acc yield acc probs that will sum to 1. so yay!!! on to perceptrons ig

---
+++ Naive Bayes Classifier/bayes.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#we are building a spam filter model given a number of emails.
+
+#data
+data = pd.read_csv("Naive Bayes Classifier/naive_bayes_spam_dataset.csv")
+X = data[['free', 'meeting', 'offer']]
+features = ['free', 'meeting', 'offer']
+y = data['class']
+class_names = data['class'].unique() #gets class names
+class_scores= {}
+
+#model
+for c in class_names:
+ count_c = (data['class'] == c).sum()
+ l_probs = np.log(count_c / len(X))
+ for f in range(len(features)):
+    col_name = features[f]
+    count_x_c = ((data['class'] == c) & (data[col_name].values == 1)).sum()
+    prob = (count_x_c + 1) / (count_c + 3)
+    l_probs += np.log(prob)
+
+ class_scores[c] = l_probs #these are logits
+
+#we create a fucntion that normalizes our logits so that they become acc probabilities that add up to 1
+def softmax(arr, not_arr):
+  score1 = np.exp(arr)
+  score2 = np.exp(not_arr)
+  total = np.sum([score1, score2])
+  prob_arr = score1 / total
+  prob_not_arr = score2 / total
+  return [prob_arr, prob_not_arr]
+scores = softmax(class_scores['spam'], class_scores['not_spam'])
+
+#print our probs for our spam filter model
+print(f"Spam: {scores[0]* 100:.2f}%")
+print(f"Not spam: {scores[1]* 100:.2f}%")
+
+#and that is a wrap for model number 4!!!!!!!
+
+  
