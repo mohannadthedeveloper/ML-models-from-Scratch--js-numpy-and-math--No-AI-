e769793526937944cfae06623f291c43b78a6a7a
Moha <flashmoah315@gmail.com>
2026-09-21 19:32:14 +0300

    Js added dataprop.py

--- Naive Bayes Classifier/bayes.py
+++ Naive Bayes Classifier/bayes.py
@@ -18,22 +18,21 @@ for c in class_names:
  for f in range(len(features)):
     col_name = features[f]
     count_x_c = ((data['class'] == c) & (data[col_name].values == 1)).sum()
-    prob = (count_x_c + 1) / (count_c + 3)
+    prob = (count_x_c + 1) / (count_c + 3) #laplace smoothing
     l_probs += np.log(prob)
 
  class_scores[c] = l_probs #these are logits
 
 #we create a fucntion that normalizes our logits so that they become acc probabilities that add up to 1
 def softmax(arr, not_arr):
-  score1 = np.exp(arr)
-  score2 = np.exp(not_arr)
+  score1, score2 = np.exp(arr), np.exp(not_arr)
   total = np.sum([score1, score2])
   prob_arr = score1 / total
   prob_not_arr = score2 / total
-  return [prob_arr, prob_not_arr]
+  return [prob_arr, prob_not_arr] #returns probs as a 1D list
 scores = softmax(class_scores['spam'], class_scores['not_spam'])
 
-#print our probs for our spam filter model
+#print our probs for our spam filter model(2 d.p js like in A level physics)
 print(f"Spam: {scores[0]* 100:.2f}%")
 print(f"Not spam: {scores[1]* 100:.2f}%")
 
