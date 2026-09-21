7e95a0443cc0287165628ebbef7bc435b77607d3
Moha <flashmoah315@gmail.com>
2026-09-21 21:01:52 +0300

    js made a .ipynb for the pipeline

--- dataprop.py
+++
@@ -1,8 +0,0 @@
-'''
-Ok I've lowk realised i need to get better at data preprocessing and cleaning,
-so this is a subquest in my ML journey, bc we lowk need to build actual pipelines as
-data wouldnt be so clean.
-'''
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
